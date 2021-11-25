da8939fe24bad1bc63cac2e2bc03ef53c1a2e0b3
DESKTOP-L4N9Q01\CREZ <crezpradhan@gmail.com>
2021-11-25 14:44:29 -0500

    Created a branch

--- src/Welcome/Welcome.java
+++ src/Welcome/Welcome.java
@@ -6,7 +6,7 @@ public class Welcome {
      * This program displays a greeting for the reader
      */
         public static void main(String[] args) {
-            String greeting = "Welcome to Core Java!";
+            String greeting = "Welcome to Core Java! Thank you for checking it out";
             System.out.println(greeting);
             for (int i = 0; i < greeting.length(); i++) {
                 System.out.print("=");
